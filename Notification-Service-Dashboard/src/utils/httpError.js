2c6b63e747b299668245adaca5eacfa5f68968ae
DashTech Computers <nmacfrancis@gmail.com>
2026-09-24 06:00:22 -0500

    Delete Notification-Service-Dashboard directory

--- Notification-Service-Dashboard/src/utils/httpError.js
+++
@@ -1,9 +0,0 @@
-// Throw from controllers/services to send a client error through errorMiddleware
-class HttpError extends Error {
-  constructor(status, message) {
-    super(message);
-    this.status = status;
-  }
-}
-
-module.exports = HttpError;
